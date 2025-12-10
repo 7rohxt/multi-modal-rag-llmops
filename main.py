9a799f6fe375cef075e484a3364aac397fbdcb78
Rohit Karthick <rkbeo5@gmail.com>
2025-12-11 00:35:00 +0530

    refactor: move model and retriever initialization outside main()

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ from src.generation import generate_answer_rag, load_generator_llm, build_contex
 from src.caching import load_redis_client, cache_get, cache_set
 from src.prompts import ROUTER_PROMPT, GENERATION_PROMPT
 
+redis_client = load_redis_client()
+router_llm = load_router_llm()
+generator_llm = load_generator_llm()
+
+embedding_model = get_embedding_model()
+vectorstore = load_faiss_index(embedding_model, index_path="faiss_index")
+all_docs = list(vectorstore.docstore._dict.values())
+bm25_retriever   = get_bm25_retriever(all_docs)
+
+reranker = load_reranker()
+
 def main (query: str):
 
     # -------------------- Router --------------------
-    redis_client = load_redis_client()
-    router_llm = load_router_llm()
-    generator_llm = load_generator_llm()
     router_decision = route_query(query, router_llm, ROUTER_PROMPT)
     if router_decision == "direct":
         assistant_response = generate_answer_chat(query, generator_llm)
-        cache_set(query, assistant_response, redis_client)
+        cache_set(query, assistant_response, redis_client)      
         return assistant_response
     
 
     # --------------------Redis Caching--------------------
-    redis_client = load_redis_client()
     cached = cache_get(query, redis_client)
     if cached:
         print("CACHE HIT")
@@ -28,15 +35,10 @@ def main (query: str):
 
 
     # --------------------Retriever--------------------
-    embedding_model = get_embedding_model()
-    vectorstore = load_faiss_index(embedding_model, index_path="faiss_index")
-    all_docs = list(vectorstore.docstore._dict.values())
-    bm25_retriever   = get_bm25_retriever(all_docs)
     retrieved_docs = retrieve_candidates(query, bm25_retriever, vectorstore, k_bm25=10, k_sem=10)
 
 
     # --------------------Reranker--------------------
-    reranker = load_reranker()
     reranked_docs = rerank_documents(query, reranker, retrieved_docs, top_k=10)
 
 
